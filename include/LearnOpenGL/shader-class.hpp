1a792e12a8cebd373689ceb9180bbe074d534fbf
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-04-14 15:49:31 -0400

    Finished first definition of the shader class

--- include/LearnOpenGL/shader-class.hpp
+++ include/LearnOpenGL/shader-class.hpp
@@ -1,5 +1,32 @@
 #ifndef F07C3395_BD14_4EBF_B170_A37E6AEB5E53
 #define F07C3395_BD14_4EBF_B170_A37E6AEB5E53
 
+#include <glad/glad.h>
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <string_view>
+
+class Shader {
+private:
+    // The ID of the shader object
+    unsigned int mID;
+    
+public:
+    // Constructor
+    Shader(std::string_view vertexPath, std::string_view fragmentPath);
+    // Use the shader program
+    void use() const;
+    // Utility functions for setting uniforms
+    void setUniform(std::string_view name, bool value) const;
+    void setUniform(std::string_view name, int value) const ;
+    void setUniform(std::string_view name, float value) const;
+    // Getter for the id
+    unsigned int getID() const;
+    // Destructor
+    ~Shader();
+};
 
 #endif /* F07C3395_BD14_4EBF_B170_A37E6AEB5E53 */
