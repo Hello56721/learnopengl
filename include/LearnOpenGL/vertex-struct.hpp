60a8f8404764890318b35535a7bcfadbacde8dd0
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-04-20 17:29:51 -0400

    Added a vertex struct

---
+++ include/LearnOpenGL/vertex-struct.hpp
@@ -0,0 +1,12 @@
+#ifndef DA8DB4A0_EC40_4184_935E_9575E121F063
+#define DA8DB4A0_EC40_4184_935E_9575E121F063
+
+#include <glm/glm.hpp>
+
+struct Vertex {
+    glm::vec3 positon;
+    glm::vec3 normal;
+    glm::vec3 texture_coordinate;
+}
+
+#endif /* DA8DB4A0_EC40_4184_935E_9575E121F063 */
