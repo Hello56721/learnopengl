e0ad3fea25677f1a78eddd83f98db6ccef9816f5
Liangyue Zhao <liangyuezhao7@gmail.com>
2021-04-20 17:31:21 -0400

    Added a struct Added a struct to represent OpenGL textures

---
+++ include/LearnOpenGL/texture-struct.hpp
@@ -0,0 +1,11 @@
+#ifndef AF75FAC3_6E4D_4BB9_9063_BFA09ADDB623
+#define AF75FAC3_6E4D_4BB9_9063_BFA09ADDB623
+
+#include <string>
+
+struct Texture {
+    unsigned int texture;
+    std::string type;
+}
+
+#endif /* AF75FAC3_6E4D_4BB9_9063_BFA09ADDB623 */
