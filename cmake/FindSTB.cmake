e2be3bd37b2565d20160122a32512b7e16b586e1
Liangyue "Yue" Zhao <Hello56721@github.com>
2021-11-18 17:42:56 -0500

    Removed the finder scripts.

--- cmake/FindSTB.cmake
+++
@@ -1,9 +0,0 @@
-find_path(
-    STB_INCLUDE_DIR
-    NAMES "stb_image.h"
-    PATHS "${CMAKE_SOURCE_DIR}/deps/stb"
-    DOC "Absolute path to STB's include directory"
-)
-
-include("FindPackageHandleStandardArgs")
-find_package_handle_standard_args(STB DEFAULT_MSG STB_INCLUDE_DIR)
